3987b6480a73c5ed0e8c9af57775dfd435ad26b8
Foxinatel <75813390+Foxinatel@users.noreply.github.com>
2022-02-03 22:32:24 +0000

    Fixed formatting

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -6,17 +6,22 @@
 
 template <char... Cs>
 struct string_literal {
-    constexpr static char value[] = {(Cs)...,0};
+    constexpr static char value[] = {(Cs)..., 0};
 };
 
-template<char... digits>
-struct to_chars { constexpr static auto value = string_literal<(digits+'0')..., '\n'>{};};
+template <char... digits>
+struct to_chars {
+    constexpr static auto value = string_literal<(digits+'0')..., '\n'>{};
+};
 
-template<char rem, char... digits>
-struct explode : explode<rem / 10, rem % 10, digits...> {};
+template <char rem, char... digits>
+struct to_string_literal : to_string_literal<rem / 10, rem % 10, digits...> {};
 
-template<char... digits>
-struct explode<0, digits...> : to_chars<digits...> {};
+template <char... digits>
+struct to_string_literal<0, digits...> : to_chars<digits...> {};
+
+template <size_t n>
+constexpr auto to_string_literal_v = to_string_literal<n>::value;
 
 template <char... Cs1, char... Cs2>
 constexpr auto operator+(string_literal<Cs1...> a, string_literal<Cs2...> b) {
@@ -26,16 +31,14 @@ constexpr auto operator+(string_literal<Cs1...> a, string_literal<Cs2...> b) {
 template <class charT = char, charT... Cs>
 constexpr auto operator""_S(){
     return string_literal<Cs..., '\n'>{};
-};
+}
 
-template<size_t n>
+template <size_t n>
 constexpr auto FizzBuzz() {
     if constexpr (Fizz(n) && Buzz(n)) return "FizzBuzz"_S;
     else if constexpr (Fizz(n)) return "Fizz"_S;
     else if constexpr (Buzz(n)) return "Buzz"_S;
-    else {
-        return explode<n>::value;
-    }
+    else return to_string_literal_v<n>;
 }
 
 int main() {
